e6cb3db853567faffbfd128098d148fa17a1596c
Jerker Sandsten <jersan@jpj.com>
2019-07-12 00:30:16 +0200

    Formatting with Prettier

--- src/plugins/globalComponents.js
+++ src/plugins/globalComponents.js
@@ -1,13 +1,13 @@
-import {Parallax} from "../components";
+import { Parallax } from "../components";
 
 /**
  * You can register global components here
  */
 
 const globalComponents = {
-    install(Vue) {
-        Vue.component(Parallax.name, Parallax);
-    }
+  install(Vue) {
+    Vue.component(Parallax.name, Parallax);
+  }
 };
 
 export default globalComponents;
--- src/router/router.js
+++ src/router/router.js
@@ -15,44 +15,44 @@ export default new Router({
     {
       path: "/",
       name: "index",
-      components: {default: Index, header: MainNavbar, footer: MainFooter},
+      components: { default: Index, header: MainNavbar, footer: MainFooter },
       props: {
-        header: {colorOnScroll: 400},
-        footer: {backgroundColor: "black"}
+        header: { colorOnScroll: 400 },
+        footer: { backgroundColor: "black" }
       }
     },
     {
       path: "/landing",
       name: "landing",
-      components: {default: Landing, header: MainNavbar, footer: MainFooter},
+      components: { default: Landing, header: MainNavbar, footer: MainFooter },
       props: {
-        header: {colorOnScroll: 400},
-        footer: {backgroundColor: "black"}
+        header: { colorOnScroll: 400 },
+        footer: { backgroundColor: "black" }
       }
     },
     {
       path: "/login",
       name: "login",
-      components: {default: Login, header: MainNavbar},
+      components: { default: Login, header: MainNavbar },
       props: {
-        header: {colorOnScroll: 400}
+        header: { colorOnScroll: 400 }
       }
     },
     {
       path: "/profile",
       name: "profile",
-      components: {default: Profile, header: MainNavbar, footer: MainFooter},
+      components: { default: Profile, header: MainNavbar, footer: MainFooter },
       props: {
-        header: {colorOnScroll: 400},
-        footer: {backgroundColor: "black"}
+        header: { colorOnScroll: 400 },
+        footer: { backgroundColor: "black" }
       }
     }
   ],
   scrollBehavior: to => {
     if (to.hash) {
-      return {selector: to.hash};
+      return { selector: to.hash };
     } else {
-      return {x: 0, y: 0};
+      return { x: 0, y: 0 };
     }
   }
 });
