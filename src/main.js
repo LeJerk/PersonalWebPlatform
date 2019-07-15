507f94efa6e8f29a86f8356325a1c5d5663a2be4
fknjerkz <jerker.sandsten@gmail.com>
2019-07-15 12:40:18 +0200

    Adjusted Vuetify import

--- src/main.js
+++ src/main.js
@@ -1,12 +1,12 @@
 import Vue from "vue";
+import "./plugins/vuetify";
 import App from "./App.vue";
 import router from "./router/router";
 import NowUiKit from "./plugins/now-ui-kit";
-import Vuetify from "vuetify";
 
 Vue.config.productionTip = false;
 
-Vue.use(NowUiKit, Vuetify);
+Vue.use(NowUiKit);
 
 new Vue({
   router,
