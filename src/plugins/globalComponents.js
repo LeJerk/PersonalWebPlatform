e9e61412384d3d99f308cef55337a686ab848854
fknjerkz <jerker.sandsten@gmail.com>
2019-07-15 11:53:25 +0200

    Added raspberry-pi icon

--- src/plugins/globalComponents.js
+++ src/plugins/globalComponents.js
@@ -2,9 +2,13 @@ import { Parallax } from "../components";
 import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 import { library } from "@fortawesome/fontawesome-svg-core";
 import { faHome } from "@fortawesome/free-solid-svg-icons";
-import { faLinkedin, faGithubSquare } from "@fortawesome/free-brands-svg-icons";
+import {
+  faLinkedin,
+  faGithubSquare,
+  faRaspberryPi
+} from "@fortawesome/free-brands-svg-icons";
 
-library.add(faHome, faLinkedin, faGithubSquare);
+library.add(faHome, faLinkedin, faGithubSquare, faRaspberryPi);
 
 /**
  * You can register global components here
