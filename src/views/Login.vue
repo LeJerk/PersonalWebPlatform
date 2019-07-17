5252fb54fcdf74a02933b50f209a4dab0ee0f46d
fknjerkz <jerker.sandsten@gmail.com>
2019-07-17 13:47:40 +0200

    Formatting and added support for color changer (emotion)

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,70 +1,81 @@
 <template>
-  <div class="page-header clear-filter" filter-color="orange">
-    <div
-      class="page-header-image"
-      style="background-image: url('img/login.jpg')"
-    ></div>
-    <div class="content">
-      <div class="container">
-        <div class="col-md-5 ml-auto mr-auto">
-          <card plain type="login">
-            <div class="logo-container" slot="header">
-              <img alt="" v-lazy="'img/now-logo.png'" />
+    <div class="page-header clear-filter" v-bind:filter-color="[emotionString]">
+        <div
+                class="page-header-image"
+                style="background-image: url('img/login.jpg')"
+        ></div>
+        <div class="content">
+            <div class="container">
+                <div class="col-md-5 ml-auto mr-auto">
+                    <card plain type="login">
+                        <div class="logo-container" slot="header">
+                            <img alt="" v-lazy="'img/now-logo.png'"/>
+                        </div>
+
+                        <fg-input
+                                addon-left-icon="now-ui-icons users_circle-08"
+                                class="no-border input-lg"
+                                placeholder="First Name..."
+                        >
+                        </fg-input>
+
+                        <fg-input
+                                addon-left-icon="now-ui-icons text_caps-small"
+                                class="no-border input-lg"
+                                placeholder="Last Name..."
+                        >
+                        </fg-input>
+
+                        <template slot="raw-content">
+                            <div class="card-footer text-center">
+                                <a
+                                        class="btn btn-primary btn-round btn-lg btn-block"
+                                        href="#pablo"
+                                >Get Started</a
+                                >
+                            </div>
+                            <div class="pull-left">
+                                <h6>
+                                    <a class="link footer-link" href="#pablo">Create Account</a>
+                                </h6>
+                            </div>
+                            <div class="pull-right">
+                                <h6>
+                                    <a class="link footer-link" href="#pablo">Need Help?</a>
+                                </h6>
+                            </div>
+                        </template>
+                    </card>
+                </div>
             </div>
-
-            <fg-input
-              addon-left-icon="now-ui-icons users_circle-08"
-              class="no-border input-lg"
-              placeholder="First Name..."
-            >
-            </fg-input>
-
-            <fg-input
-              addon-left-icon="now-ui-icons text_caps-small"
-              class="no-border input-lg"
-              placeholder="Last Name..."
-            >
-            </fg-input>
-
-            <template slot="raw-content">
-              <div class="card-footer text-center">
-                <a
-                  class="btn btn-primary btn-round btn-lg btn-block"
-                  href="#pablo"
-                  >Get Started</a
-                >
-              </div>
-              <div class="pull-left">
-                <h6>
-                  <a class="link footer-link" href="#pablo">Create Account</a>
-                </h6>
-              </div>
-              <div class="pull-right">
-                <h6>
-                  <a class="link footer-link" href="#pablo">Need Help?</a>
-                </h6>
-              </div>
-            </template>
-          </card>
         </div>
-      </div>
+        <main-footer></main-footer>
     </div>
-    <main-footer></main-footer>
-  </div>
 </template>
 <script>
-import { Button, Card, FormGroupInput } from "@/components";
-import MainFooter from "@/layout/MainFooter";
+    import {Button, Card, FormGroupInput} from "@/components";
+    import MainFooter from "@/layout/MainFooter";
+    import {EventBus} from '../event-bus.js';
 
-export default {
-  name: "login-page",
-  bodyClass: "login-page",
-  components: {
-    Card,
-    MainFooter,
-    [Button.name]: Button,
-    [FormGroupInput.name]: FormGroupInput
-  }
-};
+    export default {
+        name: "login-page",
+        bodyClass: "login-page",
+        components: {
+            Card,
+            MainFooter,
+            [Button.name]: Button,
+            [FormGroupInput.name]: FormGroupInput
+        },
+        data: function () {
+            return {
+                emotionString: 'purple'
+            };
+        },
+        mounted() {
+            EventBus.$on('emotionChanged', emotionString => {
+                this.emotionString = emotionString
+            });
+        }
+    };
 </script>
 <style></style>
