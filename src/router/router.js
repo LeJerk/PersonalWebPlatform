55b58480d8f69795767547b82e81318d175ed9fd
fknjerkz <jerker.sandsten@gmail.com>
2019-07-17 13:47:00 +0200

    Adjusted pathways to represent actual paths

--- src/router/router.js
+++ src/router/router.js
@@ -1,9 +1,8 @@
 import Vue from "vue";
 import Router from "vue-router";
 import Index from "../views/Index.vue";
-import Landing from "../views/Landing.vue";
+import Cv from "../views/Landing.vue";
 import Login from "../views/Login.vue";
-import Profile from "../views/Profile.vue";
 import MainNavbar from "../layout/MainNavbar.vue";
 import MainFooter from "../layout/MainFooter.vue";
 
@@ -22,29 +21,20 @@ export default new Router({
       }
     },
     {
-      path: "/landing",
-      name: "landing",
-      components: { default: Landing, header: MainNavbar, footer: MainFooter },
+      path: "/work/cv",
+      name: "cv",
+      components: { default: Cv, header: MainNavbar, footer: MainFooter },
       props: {
         header: { colorOnScroll: 400 },
         footer: { backgroundColor: "black" }
       }
     },
     {
-      path: "/login",
+      path: "/orbit/login",
       name: "login",
       components: { default: Login, header: MainNavbar },
       props: {
         header: { colorOnScroll: 400 }
-      }
-    },
-    {
-      path: "/profile",
-      name: "profile",
-      components: { default: Profile, header: MainNavbar, footer: MainFooter },
-      props: {
-        header: { colorOnScroll: 400 },
-        footer: { backgroundColor: "black" }
       }
     }
   ],
