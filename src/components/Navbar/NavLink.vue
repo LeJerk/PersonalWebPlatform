ff31748f6c7d43628ed36e9d9837336d141eba86
fknjerkz <jerker.sandsten@gmail.com>
2019-07-23 01:33:53 +0200

    Added emotion filter and formatting

--- src/components/Navbar/NavLink.vue
+++ src/components/Navbar/NavLink.vue
@@ -1,38 +1,56 @@
 <template>
-  <component
-    :is="componentType"
-    :to="to"
-    @click.native.stop="closeNav"
-    class="dropdown-item"
-  >
-    <slot></slot>
-  </component>
+    <component
+            :is="componentType"
+            :to="to"
+            @click.native.stop="closeNav"
+            class="dropdown-item"
+            v-bind:filter-color="[emotionString]"
+    >
+        <slot></slot>
+    </component>
 </template>
 <script>
-export default {
-  name: "nav-link",
-  inject: ["closeNavbar", "closeDropDown"],
-  props: {
-    to: {
-      type: [String, Object],
-      default: undefined
-    }
-  },
-  computed: {
-    componentType() {
-      return this.to ? "router-link" : "a";
-    }
-  },
-  methods: {
-    closeNav() {
-      if (this.closeNavbar) {
-        this.closeNavbar();
-      }
-      if (this.closeDropDown) {
-        this.closeDropDown();
-      }
-    }
-  }
-};
+    import {EventBus} from '../../event-bus.js';
+
+    export default {
+        name: "nav-link",
+        inject: ["closeNavbar", "closeDropDown"],
+        props: {
+            to: {
+                type: [String, Object],
+                default: undefined
+            }
+        },
+        data() {
+            return {
+                emotionString: 'purple'
+            };
+        },
+        computed: {
+            componentType() {
+                return this.to ? "router-link" : "a";
+            }
+        },
+        methods: {
+            closeNav() {
+                if (this.closeNavbar) {
+                    this.closeNavbar();
+                }
+                if (this.closeDropDown) {
+                    this.closeDropDown();
+                }
+            }
+        },
+        mounted() {
+            this.$nextTick(() => {
+                if (this.value) {
+                    this.findAndActivateTab(this.value);
+                }
+            });
+            EventBus.$on('emotionChanged', emotionString => {
+                this.emotionString = emotionString
+            });
+        },
+    };
 </script>
 <style></style>
