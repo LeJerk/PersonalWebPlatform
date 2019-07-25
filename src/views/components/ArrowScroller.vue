a82c0a0e311581342d56c425b7a18348aac7757a
fknjerkz <jerker.sandsten@gmail.com>
2019-07-25 02:43:13 +0200

    Formatting

--- src/views/components/ArrowScroller.vue
+++ src/views/components/ArrowScroller.vue
@@ -1,24 +1,22 @@
 <template>
-    <v-container>
-        <svg id="more-arrows" @click="$vuetify.goTo(target, options)">
-            <polygon
-                    class="arrow-top"
-                    points="37.6,27.9 1.8,1.3 3.3,0 37.6,25.3 71.9,0 73.7,1.3 "
-            />
-            <polygon
-                    class="arrow-middle"
-                    points="37.6,45.8 0.8,18.7 4.4,16.4 37.6,41.2 71.2,16.4 74.5,18.7 "
-            />
-            <polygon
-                    class="arrow-bottom"
-                    points="37.6,64 0,36.1 5.1,32.8 37.6,56.8 70.4,32.8 75.5,36.1 "
-            />
-        </svg>
-    </v-container>
+    <svg id="more-arrows" @click="$vuetify.goTo(target, options)">
+        <polygon
+                class="arrow-top"
+                points="37.6,27.9 1.8,1.3 3.3,0 37.6,25.3 71.9,0 73.7,1.3 "
+        />
+        <polygon
+                class="arrow-middle"
+                points="37.6,45.8 0.8,18.7 4.4,16.4 37.6,41.2 71.2,16.4 74.5,18.7 "
+        />
+        <polygon
+                class="arrow-bottom"
+                points="37.6,64 0,36.1 5.1,32.8 37.6,56.8 70.4,32.8 75.5,36.1 "
+        />
+    </svg>
 </template>
 <script>
     export default {
-        data () {
+        data() {
             return {
                 duration: 600,
                 offset: 0,
@@ -26,17 +24,17 @@
             }
         },
         computed: {
-            target () {
+            target() {
                 return this['element'];
             },
-            options () {
+            options() {
                 return {
                     duration: this.duration,
                     offset: this.offset,
                     easing: this.easing
                 }
             },
-            element () {
+            element() {
                 return this.$parent.$refs.profile;
             }
         }
